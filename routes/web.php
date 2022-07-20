8bcae67242b1b6fc7b119503ee769ee3d410a371
akihiro.ikawa <ikawa-akihiro@agex.co.jp>
2022-07-20 06:47:27 +0000

    ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@
 
 Route::get('/', 'TasksController@index');
 
-Route::resource('tasks', 'TasksController');
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('tasks', 'TasksController');
+});
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
